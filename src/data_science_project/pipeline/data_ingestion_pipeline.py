3f000cdaec65a74cb7d88b22b087456e82be0bcb
Gajender <Iamsanju0707@gmail.com>
2024-12-03 09:57:17 +0530

    adding the files

---
+++ src/data_science_project/pipeline/data_ingestion_pipeline.py
@@ -0,0 +1,12 @@
+from src.data_science_project.components.data_ingestion import DataIngestion
+from src.data_science_project.config.configuration import ConfigurationManager
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_data_ingestion(self):
+        pass
+
+    
